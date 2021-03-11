5a98d9f45fad410e88a80c67c745772a30dd8346
nikita <prokayev@icloud.com>
2021-03-12 00:31:23 +0100

    added basic functionality

--- misc.py
+++ misc.py
@@ -9,13 +9,13 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 load_dotenv(find_dotenv())
 
-#TOKEN = os.environ['TOKEN']
-TOKEN = os.environ['TEST_TOKEN']
+TOKEN = os.environ['TOKEN']
+#TOKEN = os.environ['TEST_TOKEN']
 
 BSC_SCAN_API_KEY = os.environ['BSC_SCAN_API_KEY']
 
-#WEBHOOK_HOST = 'https://safemoonbot.herokuapp.com'
-WEBHOOK_HOST = "https://468ddd420a3f.ngrok.io"
+WEBHOOK_HOST = 'https://safemoonbot.herokuapp.com'
+#WEBHOOK_HOST = "https://468ddd420a3f.ngrok.io"
 WEBHOOK_PATH = '/webhook/'
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
